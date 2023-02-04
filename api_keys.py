a797881df7143f5dfff6d17be5a7f897a6611587
bennettnorth <46634401+bennettnorth@users.noreply.github.com>
2023-02-04 14:48:48 -0500

    test

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "4667829335f3f675f6a15b53f661b8f2"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "01823aac740846168fc67faa0f599891"
